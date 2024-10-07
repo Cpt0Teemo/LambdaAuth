734db4954eaeb59cb990929312bb28c788ad4c9f
Cpt0Teemo <ypoulmarck@gmail.com>
2024-10-07 20:54:53 +0100

    Working placeholder login page

--- app/Main.hs
+++ app/Main.hs
@@ -6,16 +6,18 @@ import Web.Scotty
 import Database.PostgreSQL.Simple
 import Debug.Trace
 import qualified Data.Text.Lazy as L
+import qualified Data.Text.Lazy.IO as LIO (readFile)
 
 
-main = scotty 3000 $
+main = scotty 3000 $ do
     get "/user" $ do
         conn  <- liftIO . connect $ localPG
         email <- liftIO . test $ conn
         html $ mconcat ["<h1>Scotty, ", L.fromStrict email, " me up!</h1>"]
-    -- get "/:word" $ do
-    --     beam <- pathParam "word"
-    --     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+
+    get "/login" $ do
+        loginPage <- liftIO . LIO.readFile $ "app/LoginPage.html"
+        html loginPage
 
 localPG :: ConnectInfo
 localPG = defaultConnectInfo
